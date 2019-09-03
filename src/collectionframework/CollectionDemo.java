2735da44a47ad108b4b0b1a1fea1f710fe993d89
melayer-01 <melayer.rajan@gmail.com>
2019-09-03 10:56:14 +0530

    map.

--- src/collectionframework/CollectionDemo.java
+++ src/collectionframework/CollectionDemo.java
@@ -1,30 +1,59 @@
 package collectionframework;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CollectionDemo {
-
+    List<String> list=new ArrayList<>();
+    List<String> list1=new ArrayList<>();
     void listData(){
-        String name="rajan,shital,swaraj,priyanka,codekul";
-
-        List<String> list=new ArrayList<>();
-
-        list.add(name);
-
-        /*list.add("Rajan");
+        //String name="rajan,shital,swaraj,priyanka,codekul";
+        list.add("rajan");
         list.add("shital");
         list.add("swaraj");
         list.add("priyanka");
         list.add(1,"codekul");
-*/
-        for (String s:list) {
+        list.add("rajan");
+        list.add("shital");
+        list.add("swaraj");
+        list.add("priyanka");
+        list.add("rajan");
+        list.add("shital");
+        list.add("swaraj");
+        list.add("priyanka");
+
+        list1.add("codekul");
+        list1.addAll(list);
+/*
+        for (String s:list1) {
             System.out.println(s);
         }
+
+        System.out.println("Set Data  :");*/
+
+      setData(list1);
+        }
+
+        void setData(List<String> list){
+
+            Set<String> set=new TreeSet<>();
+            set.addAll(list);
+            for (String s:set) {
+                System.out.println(s);
+            }
+
         }
 
 
+        void setMapData(){
+
+        Map<String,Object> map=new HashMap<>();
+        map.put("name","Rajan");
+        map.put("address","Pune");
+        map.put("mobileNumber","9960448507");
+            System.out.println(map.get("mobileNumber"));
+        }
+
     public static void main(String[] args) {
-        new CollectionDemo().listData();
+        new CollectionDemo().setMapData();
     }
 }
